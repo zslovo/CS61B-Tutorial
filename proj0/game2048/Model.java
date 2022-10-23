cf98e93464a5761fafc4849888630f0f11425356
zslovo <664657095@qq.com>
2022-10-23 23:00:33 +0800

    Fill In EmptySpace Function

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -137,7 +137,16 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        for(int i = 0; i < 4; i += 1)
+        {
+            for (int j = 0; j < 4; j += 1)
+            {
+                if (b.tile(i, j) == null)
+                {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
