dd8af4ef8a8a5501fe7fc422ae7bc0fe722ac490
zslovo <664657095@qq.com>
2022-11-07 18:05:13 +0800

    Finish lab3

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,15 @@
+package deque;
+
+public class LinkedListDeque {
+    public class IntNode
+    {
+        int item;
+        IntNode next;
+
+        public IntNode(int i, IntNode n)
+        {
+            item = i;
+            next = n;
+        }
+    }
+}
